627bc7c00f5b9295289e344a297a1750d1422102
onam <onam75hernandez@gmail.com>
2023-12-24 00:58:15 -0800

    Added fugitive, codieum, and hard-time plugins

---
+++ lua/plugins/hard-time.lua
@@ -0,0 +1,8 @@
+return {
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	config = function()
+		require("hardtime").setup({})
+	end,
+	event = "BufEnter",
+}
