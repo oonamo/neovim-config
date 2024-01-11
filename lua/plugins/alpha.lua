90de44cbdeff038f61269ac21c8b591f05547440
onam <onam75hernandez@gmail.com>
2024-01-10 22:44:14 -0800

    rebase

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,9 +7,9 @@ return {
 		local startify = require("alpha.themes.startify")
 
 		startify.section.top_buttons.val = {
-			startify.button("e", "∩à¢  New file", ":ene <BAR> startinsert <CR>"),
-			startify.button("o", "∩à¢  Obsidian", ":cd C:/Users/onam7/Desktop/o notes/ | e . <CR>"),
-			startify.button("q", "≤░àÜ  Quit NVIM", ":qa<CR>"),
+			startify.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			startify.button("o", "  Obsidian", ":cd /mnt/c/Users/onam7/Desktop/o notes/ | e . <CR>"),
+			startify.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
 		}
 
 		alpha.setup(startify.config)
