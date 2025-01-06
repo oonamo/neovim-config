0182ca3fb3a07edad1d7fe63470ad45550136f12
oonamo <onam75hernandez@gmail.com>
2025-01-05 19:14:31 -0800

    testing

--- init.lua
+++ init.lua
@@ -363,9 +363,7 @@ later(function()
   vim.api.nvim_create_autocmd("User", {
     pattern = "MiniGitCommandDone",
     callback = function(ev)
-      vim.notify("command done!")
-      vim.print(ev)
-      if ev.data.git_subcommand:match("status") then
+      if ev.data.git_subcommand:match("status") and ev.data.stdout ~= "" then
         Config._cache.git = vim.b.minigit_summary
         vim.api.nvim_create_autocmd("User", {
           nested = true,
